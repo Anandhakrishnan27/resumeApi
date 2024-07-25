a9a00773ddc7faa2c405f4b05e42c44bb3a69aa0
Anandhakrishnan Senthooraan <anandhakrishnansenthooraan@Anandhakrishnans-MacBook-Pro.local>
2024-07-24 18:18:50 -0700

    aws best practice

--- bin/resume-api-cdk.ts
+++ bin/resume-api-cdk.ts
@@ -6,7 +6,6 @@ import { ResumeApiCdkStack } from '../lib/resume-api-cdk-stack';
 const app = new cdk.App();
 new ResumeApiCdkStack(app, 'ResumeApiCdkStack', {
   env: { 
-    account: '891377376926', 
     region: 'us-east-1'
   },
 });
